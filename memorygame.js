24bf258f720ae8b141b6365b5ff4f29810c43356
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2021-10-18 16:55:12 +0200

    js finished
    last commit

--- memorygame.js
+++ memorygame.js
@@ -4,22 +4,71 @@
      const cards = document.querySelectorAll(".memory-card");
 
      let hasFlippedcard = false;
-     let firstCard, SecondCard;
+     let lockBoard = false;
+     let firstCard, secondCard;
 
      function flipcard(){
+        if(lockBoard)  return;
+        if(this === firstCard) return;
         this.classList.add('flip');
+        
 
         if(!hasFlippedcard){
             hasFlippedcard = true;
             firstCard = this;
         }else{
-            hasFlippedcard = false;
-            SecondCard = this;
+            secondCard = this;
+            checkForMatch(); 
+        }
+        
+
+
+    }
+
+        
+        function checkForMatch(){
+
+            if(firstCard.dataset.framework === secondCard.dataset.framework){
+                disableCards();
+            }else{
+                unflipCards();
+            }
+
         }
 
-        console.log(firstCard.dataset.framework);
-        console.log(SecondCard.dataset.framework);
-     }
+        function disableCards(){
+            firstCard.removeEventListener('clcik',flipcard);
+            secondCard.removeEventListener('click', flipcard);
+
+            resetBoard();
+        }
+
+        function unflipCards(){
+            lockBoard = true;
+
+            setTimeout(()=>{
+                
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+
+                resetBoard();
+
+                }, 1500);
+        }
+
+        function resetBoard(){
+            [hasFlippedcard, lockBoard] = [false, false];
+            [firstCard, secondCard] = [null, null];
+        }
+     
+        (function shuffle(){
+            cards.forEach(card=>{
+            let randomPos = Math.floor(Math.random() * 12);
+            card.style.order = randomPos;
+            });
+        })();
+
+        
 
      cards.forEach(card =>card.addEventListener("click", flipcard));
    
